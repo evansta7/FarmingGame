5c4d3598b72dc538df41a1c31d16057e5f246ba9
evansta7 <lizy.vs@gmail.com>
2018-05-12 09:45:06 +0200

    dummy login code

--- FarmingGame/Form1.cs
+++ FarmingGame/Form1.cs
@@ -15,12 +15,32 @@ namespace FarmingGame
         public Form1()
         {
             InitializeComponent();
-            //o
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-         
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+            if (username == "admin" && password == "password")
+            {
+                MessageBox.Show(string.Format("you are now logged in as admin. Welcome {0}",username));
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+
+            }
+            else if (username == "administrator" && password == "administratorPassword") {
+                MessageBox.Show(string.Format("you are now logged in as administrator. Welcome {0}", username));
+                Form2 form2 = new Form2();
+                form2.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login failed, please try again");
+            }
+
+
 
         }
 
