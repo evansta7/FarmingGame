23c7d9d74007d7b5f6d34051ff048bfc17018a3b
Anelle <39213526+QueenOfChaos2811@users.noreply.github.com>
2018-05-17 15:12:02 +0200

    RegistrationForm creation, i only added a username, password and emila verification because we dont really need anything else.

--- FarmingGame/Form2.cs
+++ FarmingGame/Form2.cs
@@ -16,5 +16,42 @@ namespace FarmingGame
         {
             InitializeComponent();
         }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUsername.Clear();
+            txtPassword.Clear();
+            txtEmail.Clear();
+            txtEmailVer.Clear();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileHandler fh = new FileHandler(fileName);
+                List<string> li = new List<string>();
+                string username = txtUsername.Text;
+                string password = txtPassword.Text;
+                string email = txtEmail.Text;
+                string emailVer = txtEmailVer.Text;
+
+                li.Add(string.Format("{0} {1} {2} {3}", username, password, email, emailVer));
+
+
+                fh.WriteData(li);
+                MessageBox.Show("Registration Complete!", "Congratulations", MessageBoxButtons.OK);
+
+            }
+            finally
+            {
+                
+            }
+        }
     }
 }
